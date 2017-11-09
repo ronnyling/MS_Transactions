8219d987978dbaf013d58e8b13f5e1bf6acda7a9
ronnyling <ronnyckling@gmail.com>
2017-11-09 18:50:48 +0800

    v2
    cleaner codes

---
+++ Practice_MicroservicesContd-master/complete/src/main/java/hello/Mapper/SalesMapper.java
@@ -0,0 +1,20 @@
+package hello.Mapper;
+
+import hello.SalesRecord;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SalesMapper implements RowMapper<SalesRecord> {
+
+    @Override
+    public SalesRecord mapRow(ResultSet rs, int i) throws SQLException {
+        SalesRecord ads = new SalesRecord();
+        ads.setTransactionNo(rs.getInt("transaction_no"));
+        ads.setTransactionAmt(rs.getInt("transaction_amt"));
+        ads.setSalesPerson(rs.getString("sales_person"));
+        ads.setTransactionName(rs.getString("transaction_name"));
+        return ads;
+    }
+}
